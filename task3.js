53b19d626b13125331b19ff5224f19ac54aa6246
MistyIslam5 <mdmisty5@gmail.com>
2024-08-31 15:51:36 +0600

    task3 complete

---
+++ task3.js
@@ -0,0 +1,9 @@
+const bookName = ['Bangla', 'english', 'math', 'chemistry', 'biology', 'islam', 'javascript'];
+
+console.log(bookName.includes('javascript'));
+
+if(bookName.includes('javascript')){
+    console.log('JavaScript is present in the array.');
+}else{
+    console.log('JavaScript is not present in the array.');
+}
