3be0d1c9bff1b833708cd83ff0ac6c21a21583d6
MistyIslam5 <mdmisty5@gmail.com>
2024-08-31 16:30:37 +0600

    all task complet

---
+++ task5.js
@@ -0,0 +1,5 @@
+const friendName = ['Miaty', 'Milon', 'Shakil', 'Ziyad', 'Rukon'];
+const bookName = ['bangla', 'English', 'Mathematical', 'Chemistry'];
+
+var arry = friendName.concat(bookName);
+console.log(arry);
