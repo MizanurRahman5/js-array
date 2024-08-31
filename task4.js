83be1a8d37f8e3c179fe76a5eb2fb2841d0b087f
MistyIslam5 <mdmisty5@gmail.com>
2024-08-31 16:03:57 +0600

    task4 complet

---
+++ task4.js
@@ -0,0 +1,60 @@
+const array1 = [];
+const array2 = ['a', 'b', 'c'];
+const array3 = [true, false];
+const array4 = [23, 23, 45, 65,];
+const array5 = '';
+const array6 = 'fguoityrutgdf'
+const array7 = true;
+
+console.log(Array.isArray(array1));
+
+// array 1
+if (Array.isArray(array1)) {
+    console.log('it is array');
+    
+}else {
+    console.log('it is not array');
+}
+// array 2
+if (Array.isArray(array2)) {
+    console.log('it is array');
+    
+}else {
+    console.log('it is not array');
+}
+// array 3
+if (Array.isArray(array3)) {
+    console.log('it is array');
+}else{
+    console.log('it is not array');
+}
+// array 4 
+if (Array.isArray(array4)) {
+    console.log('it is array');
+}else {
+    console.log('it is not array');
+    
+}
+// array 5 
+if (Array.isArray(array5)) {
+    console.log('it is array');
+    
+}else {
+    console.log('it is not array');
+    
+}
+// array 6
+if (Array.isArray(array6)) {
+    console.log('it is array');
+}else {
+    console.log('it is not array');
+    
+}
+
+// array 7
+
+if (Array.isArray(array7)) {
+    console.log('it is array');
+}else {
+    console.log('it is not array');
+}
